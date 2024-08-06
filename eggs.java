ec304c7746bd2e19c5624b5efd06604505315ca3
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-06 07:06:47 +0530

    New Program of calculating Gross Dozen Left eggs

---
+++ eggs.java
@@ -0,0 +1,12 @@
+package JavaProgs;
+class eggs
+{
+    public static void main(String args[])
+    {
+        int n=1342;
+        int gross=n/144;
+        int dozen=(n%144)/12;
+        int left=(n%144)%12;
+        System.out.println("The user has "+n+" number of eggs which constitute of "+gross+" gross "+dozen+" dozen "+left+" left eggs");
+    }
+}
