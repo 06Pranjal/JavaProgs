44913a05b4239140cf07a82f11171f34de02ae33
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-09 00:52:29 +0530

    new program of deleting an element from a position in  array

---
+++ deletion.java
@@ -0,0 +1,39 @@
+package JavaProgs;
+import java.io.*;
+public class deletion
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        int arr[],size,pos,index,arr2[],arr3[];
+        System.out.println("Enter the size of array");
+        size=Integer.parseInt(in.readLine());
+        arr=new int[size];
+        System.out.println("Enter the elements");
+        arr2=new int[size-1];
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=Integer.parseInt(in.readLine());
+        }
+        System.out.println("Enter the position of element to be deleted");
+        pos=Integer.parseInt(in.readLine());
+        index=pos-1;
+        for(int i=0;i<index;i++)
+        {
+            arr2[i]=arr[i];
+        }
+        for(int i=index;i<size-1;i++)
+        {
+            arr2[i]=arr[i+1];
+        }
+
+//printing the array
+        System.out.println("Array after deletion:-");
+        for(int i=0;i<size-1;i++)
+        {
+            System.out.println(arr2[i]);
+        }
+    }
+}
+
+
