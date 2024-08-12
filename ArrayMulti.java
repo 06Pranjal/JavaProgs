19ec2cce9fc0090bf9086c447184795c8abb4761
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-12 09:53:13 +0530

    Modification in program

--- ArrayMulti.java
+++ ArrayMulti.java
@@ -1,6 +1,5 @@
-package JavaProgs;
 
-//declare a 2d array of 2*2 and perform operation like search,traversal,sum of all elements ,insertions of elements at given location,deletion of a row
+//declare a 2d array of 4*4 and perform operation like search,traversal,sum of all elements ,insertions of elements at given location,deletion of a row
 import java.io.*;
 class ArrayMulti
 {
@@ -22,11 +21,29 @@ class ArrayMulti
                 a[i][j]=Integer.parseInt(in.readLine());
             }
         }
-        System.out.println("Enter 1 for printing the array, 2 for searching,3 for Printing sum of numbers,4 for insertion of elements");
+        System.out.println("Enter 1 for searching, 2 for printing the array,3 for Printing sum of numbers,4 for insertion of elements,5 for deletion of row");
         ch=Integer.parseInt(in.readLine());
-        switch(ch) {
+        switch(ch)
+        {
             case 1:
+                System.out.println("Your entered choice is "+ch);
+                //searching the element
+                System.out.println("Enter num to be searched");
+                num = Integer.parseInt(in.readLine());
+                for (int i = 0; i < r; i++) {
+                    for (int j = 0; j < c; j++) {
+                        if (a[i][j] == num)
+                            System.out.println("Number is found at position number (" + i + "," + j + ") index");
+
+                    }
+
+                }
+                break;
+
+            case 2:
+                System.out.println("Your entered choice is "+ch);
                 //printing the array
+                System.out.println("The array is as follows");
                 for (int i = 0; i < r; i++) {
                     for (int j = 0; j < c; j++) {
                         System.out.print(a[i][j] + " ");
@@ -34,21 +51,9 @@ class ArrayMulti
                     System.out.println();
                 }
                 break;
-            case 2:
-                //searching the element
-                System.out.println("Enter num to be searched");
-                num = Integer.parseInt(in.readLine());
-                for (int i = 0; i < r; i++) {
-                    for (int j = 0; j < c; j++) {
-                        if (a[i][j] == num)
-                            System.out.println("Number is found at position number" + i + "," + j + " index");
-                        else if (a[i][j] != num)
-                            System.out.println("Number is not found");
-                    }
 
-                }
-                break;
             case 3:
+                System.out.println("Your entered choice is "+ch);
                 //sum of all elements
                 for (int i = 0; i < r; i++) {
                     for (int j = 0; j < c; j++) {
@@ -58,10 +63,11 @@ class ArrayMulti
                 System.out.println("Sum of all the elements are:" + sum);
                 break;
             case 4:
+                System.out.println("Your entered choice is "+ch);
                 // Insertion of element
-                System.out.println("Enter the row index (0 or 1) to insert an element:");
+                System.out.println("Enter the row index  to insert an element:");
                 int row = Integer.parseInt(in.readLine());
-                System.out.println("Enter the column index (0 or 1) to insert an element:");
+                System.out.println("Enter the column index to insert an element:");
                 int col = Integer.parseInt(in.readLine());
                 System.out.println("Enter the element to be inserted:");
                 int element = Integer.parseInt(in.readLine());
@@ -72,6 +78,7 @@ class ArrayMulti
                 } else {
                     System.out.println("Invalid row or column index!");
                 }
+                System.out.println("The array is as follows");
 
                 for (int i = 0; i < r; i++) {
                     for (int j = 0; j < c; j++)
@@ -81,10 +88,40 @@ class ArrayMulti
                     System.out.println();
                 }
                 break;
+            case 5:
+                System.out.println("Your entered choice is "+ch);
+                // Deletion of a row
+                System.out.println("Enter the row index to delete:");
+                int delRow = Integer.parseInt(in.readLine());
+                if (delRow >= 0 && delRow < r) {
+                    int[][] newArray = new int[r - 1][c];
+                    for (int i = 0, k = 0; i < r; i++) {
+                        if (i == delRow) {
+                            continue;
+                        }
+                        for (int j = 0; j < c; j++) {
+                            newArray[k][j] = a[i][j];
+                        }
+                        k++;
+                    }
+                    a = newArray;
+                    r--;
+                    System.out.println("Row deleted successfully.");
+                } else {
+                    System.out.println("Invalid row index!");
+                }
+                System.out.println("The new array is as follows");
+                for (int i = 0; i < r; i++) {
+                    for (int j = 0; j < c; j++) {
+                        System.out.print(a[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+                break;
             default:
                 System.out.println("Invalid choice");
         }
-        }
+    }
 
 
 
