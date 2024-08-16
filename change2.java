0a3a2830594ceec48a2b0b361594e0aff9903e89
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-16 16:49:52 +0530

    method to return the string in capital letter

---
+++ change2.java
@@ -0,0 +1,14 @@
+package JavaProgs;
+
+public class change2 {
+    String name(String n)
+    {
+        return n.toUpperCase();
+    }
+    public static void main(String args[])
+    {
+        change2 ob=new change2();
+        System.out.println(ob.name("pranjal"));
+
+    }
+}
