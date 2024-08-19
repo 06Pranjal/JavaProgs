2a842b9b356819d042965107342b2b6a256c4438
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-19 23:25:48 +0530

    Program to calculate maximum wealth

---
+++ wealth.java
@@ -0,0 +1,30 @@
+package JavaProgs;
+
+class wealth {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+
+        for(int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for(int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            // Update maxWealth if current sum is greater
+            if(sum > maxWealth) {
+                maxWealth = sum;
+            }
+        }
+
+        return maxWealth;
+    }
+
+    public static void main(String[] args) {
+        wealth ob = new wealth();
+        int[][] accounts = {
+                {1, 2, 3},
+                {3, 2, 1}
+        };
+        System.out.println(ob.maximumWealth(accounts)); // Expected output: 6
+    }
+}
+
