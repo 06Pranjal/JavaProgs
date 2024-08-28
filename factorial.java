8e0642439e4049e5ff090763aaec7d9ea3ac4189
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-28 14:57:05 +0530

    Program for factorial

---
+++ factorial.java
@@ -0,0 +1,18 @@
+package JavaProgs;
+
+public class factorial {
+    int value=1;
+    int fact(int n){
+        while(n!=0){
+            value=value*n;
+            fact(n-1);
+        }
+        return value;
+    }
+
+    public static void main(String args[]){
+        factorial ob=new factorial();
+        System.out.println("Factorial of given number is "+ob.fact(3));
+    }
+}
+
