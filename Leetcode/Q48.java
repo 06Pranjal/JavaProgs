01378ce91c5c7ab52f5a808f811c13e4950cfa60
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-09-18 19:58:18 +0530

    Rotate the array by 90 degree and print it

---
+++ Leetcode/Q48.java
@@ -0,0 +1,43 @@
+package JavaProgs.Leetcode;
+
+class Q48 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        // Print the rotated matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        Q48 ob = new Q48();
+        ob.rotate(arr);
+    }
+}
+
