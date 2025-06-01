97b96d7db304216e1892277a8c12ef28da8d6305
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-06-01 22:59:05 +0530

    Streak

---
+++ src/JavaProgs/DSA_Scratch/functiions/rotateArray.java
@@ -0,0 +1,21 @@
+package JavaProgs.DSA_Scratch.functiions;
+
+public class rotateArray {
+    void rotate(int a[],int k){
+        int n=a.length;
+        int temp[]=new int[n];
+        k=k%n;
+        for(int i=0;i<n;i++){
+            temp[(i+k)%n]=a[i];
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(temp[i]+" ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        rotateArray ob =new rotateArray();
+        ob.rotate(new int[]{1,2,3,4,5,6},3);
+    }
+}
